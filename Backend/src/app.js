111d7104a243247a39ae40c512e5c7c50cbdd720
Harsh saxena <harshsaxena427@gmail.com>
2024-05-25 22:19:51 +0530

    First commit

---
+++ Backend/src/app.js
@@ -0,0 +1,13 @@
+const express=require('express')
+
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.send('heyy')
+})
+
+
+const Port=8000
+app.listen(Port,()=>{
+    console.log('Listening to port',Port)
+})
